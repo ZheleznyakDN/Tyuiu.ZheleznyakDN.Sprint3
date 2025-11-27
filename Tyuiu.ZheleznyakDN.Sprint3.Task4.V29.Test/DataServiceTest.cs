6f0c09bf8d58ee5ae08326d3a0c69d2a5fe5d454
Железняк Дамир <Tyuiu.ZheleznyakDN@outlook.com>
2025-11-27 16:40:03 +0500

    Task4

---
+++ Tyuiu.ZheleznyakDN.Sprint3.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.ZheleznyakDN.Sprint3.Task4.V29.Lib;
+namespace Tyuiu.ZheleznyakDN.Sprint3.Task4.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double result = ds.Calculate(-5, 5);
+            double wait = 0;
+            for (int x = -5; x <= 5; x++)
+            {
+                if (x == 0) continue;
+                wait += Math.Cos(x) / x;
+            }
+            wait = Math.Round(wait, 3);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.ZheleznyakDN.Sprint3.Task4.V29/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.ZheleznyakDN.Sprint3.Task4.V29.Lib;
+namespace Tyuiu.ZheleznyakDN.Sprint3.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Железняк Д.Н. | ПКТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах               *");
+            Console.WriteLine("* Задание #4                                                            *"); 
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Железняк Дамир Николаевич | ПКТб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где х принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=cos(x)/x. При x = 0 пропустить значение.                     *");
+            Console.WriteLine("* Полученные значения суммировать.                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine($"Стартовое значение x = {startValue}");
+            Console.WriteLine($"Конечное значение x = {stopValue}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.Calculate(startValue, stopValue);
+            Console.WriteLine($"Сумма значений функции = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
